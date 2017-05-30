3aa32e6fb284a6a3f66c048dca3e5f63238d85bd
Jonny Dalgleish <fighella@gmail.com>
2017-05-30 11:51:51 +1000

    Variant name

--- lib/gia_order_tagging.rb
+++ lib/gia_order_tagging.rb
@@ -6,7 +6,11 @@ require "tag/gia_order_tag"
 
   def self.update_all_orders(tag,opts)
     # tag = 'Some Tag'
-    # opts = { title: 'One',sku: 'Sku', }
+    # opts = { title: 'One',sku: 'Sku', variant_name: '' }
+    shop = Shop.find_by(shopify_domain: 'i-am-gia.myshopify.com')
+    session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
+    ShopifyAPI::Base.activate_session(session)
+
     GiaOrder.process_all_orders(tag,opts)
   end
 
