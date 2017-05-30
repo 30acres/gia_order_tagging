1e4147bb109b70e339662733e7d2069b00851b71
Jonny Dalgleish <fighella@gmail.com>
2017-05-30 11:58:05 +1000

    Connect

--- lib/gia_order_tagging.rb
+++ lib/gia_order_tagging.rb
@@ -5,13 +5,23 @@ require "tag/gia_order_tag"
 
 
   def self.update_all_orders(tag,opts)
+    preorders = [
+      { sku: 'TM3749RED', title: 'Delevingne Hoodie'},
+      { sku: 'GIA3754SIL', title: 'Sienna Skirt'},
+      { sku: 'GIA3775SIL', title: 'Sienna Top'},
+      { sku: 'GIA3775BLK', title: 'Twiggy Top'},
+      { sku: 'GIA3541BLK', title: 'Tyler Denim Jacket'},
+      { sku: 'GIA3836NUD', title: 'Kentley Bodysuit'}
+    ]
+
     # tag = 'Some Tag'
     # opts = { title: 'One',sku: 'Sku', variant_name: '' }
     shop = Shop.find_by(shopify_domain: 'i-am-gia.myshopify.com')
     session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
     ShopifyAPI::Base.activate_session(session)
-
-    GiaOrder.process_all_orders(tag,opts)
+    pre_orders.each do |po|
+      GiaOrder.process_all_orders("#{po[:title]} (Pre Order #{Time.zone.now.strftime('%m/%y')})", po)
+    end
   end
 
   def self.update_recent_orders(tag,opts)
